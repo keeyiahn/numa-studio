6092aa1561d632a5e786152af317669f38552b6a
keeyiahn <notthad@gmail.com>
2025-12-03 12:52:31 +0800

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { 
-  ReactFlowProvider
+  ReactFlowProvider,
  } from '@xyflow/react';
+import { useEffect } from 'react';
 import Canvas from './components/Canvas';
 import Sidebar from './components/Sidebar';
 import ConfigModal from './components/ConfigModal';
@@ -16,17 +17,30 @@ export default function App() {
   const templatesHook = useTemplates();
   const modalHook = useModal();
 
+  useEffect(() => {
+    console.log("Pipeline nodes:", pipelineHook.nodes);
+    console.log("Pipeline edges:", pipelineHook.edges);
+  });
+
+
  
   return (
     <div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
-      <ConfigModal modalHook={modalHook}/>
+      <ConfigModal 
+        modalHook={modalHook}
+        pipelineHook={pipelineHook}
+        templatesHook={templatesHook}
+      />
       <Sidebar 
         templatesHook={templatesHook}
         modalHook={modalHook}
       />
       <div style={{flex: 1, height: '100%'}}>
         <ReactFlowProvider>
-          <Canvas pipeline={pipelineHook}/>
+          <Canvas 
+            pipelineHook={pipelineHook}
+            modalHook={modalHook}
+          />
         </ReactFlowProvider>
       </div>
     </div>
